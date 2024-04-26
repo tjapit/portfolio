5abae7443049c3507ab221d5ca5b3e30afb38c5e
Timothy Japit <timothy.japit@gmail.com>
2024-04-26 12:29:56 +0700

    feat: Intro section avatar and text

---
+++ components/intro.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+export default function Intro() {
+  return (
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.2,
+            }}
+          >
+            <Image
+              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
+              alt="Tim portrait"
+              width="192"
+              height="192"
+              quality="95"
+              priority={true}
+              className="h-24 w-24 object-cover rounded-full border-[.35rem] border-white shadow-xl"
+            />
+          </motion.div>
+          <motion.span
+            className="absolute bottom-0 right-0 text-4xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+
+      <motion.p
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <b>Hello, I'm Tim.</b> I'm a <b>full-stack developer</b> with{" "}
+        <b>3 years</b> of experience. I enjoy building <em>sites & apps</em>. My
+        focus is <u>React & Go.</u>
+      </motion.p>
+    </section>
+  );
+}
