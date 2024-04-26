1b89030fa6b76b5bfbc6d1f58c0a38455661cd85
Timothy Japit <timothy.japit@gmail.com>
2024-04-26 13:59:50 +0700

    feat: my own avatar

--- components/intro.tsx
+++ components/intro.tsx
@@ -21,11 +21,10 @@ export default function Intro() {
             }}
           >
             <Image
-              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
+              src="/tim.jpg"
               alt="Tim portrait"
               width="192"
               height="192"
-              quality="95"
               priority={true}
               className="h-24 w-24 object-cover rounded-full border-[.35rem] border-white shadow-xl"
             />
