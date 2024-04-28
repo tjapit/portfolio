fbc1835b4507e60d98e0f3c117fdb27e64c1cd36
Timothy Japit <timothy.japit@gmail.com>
2024-04-28 22:09:34 +0700

    feat: Finish header, scroll to section from navbar and add motion to projects

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,11 +1,22 @@
+"use client";
+
 import React from "react";
 import { projectsData } from "@/lib/data";
 import SectionHeading from "./section-heading";
 import Project from "./project";
+import { motion } from "framer-motion";
 
 export default function Projects() {
   return (
-    <section>
+    <motion.section
+      id="projects"
+      className="scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        delay: 0.2,
+      }}
+    >
       <SectionHeading>My Projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
@@ -14,6 +25,6 @@ export default function Projects() {
           </React.Fragment>
         ))}
       </div>
-    </section>
+    </motion.section>
   );
 }
