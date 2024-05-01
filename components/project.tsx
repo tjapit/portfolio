560eff25f798ba7733f689171ab374f953c35ff4
Timothy Japit <timothy.japit@gmail.com>
2024-05-02 04:29:00 +0700

    fix: non-responsive Projects on smaller screens

--- components/project.tsx
+++ components/project.tsx
@@ -34,13 +34,13 @@ export default function Project({
     >
       <section
         className="relative max-w-[42rem] bg-gray-100 border 
-      border-black/5 rounded-xl overflow-hidden sm:pr-8 sm:h-[20rem] 
-      group-even:pl-8 hover:bg-gray-200 transition duration-500
-      dark:bg-white/10 dark:hover:bg-white/20"
+        border-black/5 rounded-xl overflow-hidden sm:pr-8 sm:h-[20rem] 
+        sm:group-even:pl-8 hover:bg-gray-200 transition duration-500
+        dark:bg-white/10 dark:hover:bg-white/20"
       >
         <div
           className="pt-4 pb-7 px-5 h-full flex flex-col 
-          sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] group-even:ml-[18rem]"
+          sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] sm:group-even:ml-[18rem]"
         >
           <h3 className="text-2xl">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
@@ -64,20 +64,21 @@ export default function Project({
           alt={`Project ${title}`}
           quality={95}
           className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl 
-        transition
-        duration-300
-        group-even:right-[initial] 
-        group-even:-left-40 
+          hidden sm:block
+          transition
+          duration-300
+          group-even:right-[initial] 
+          group-even:-left-40 
 
-        group-hover:-translate-x-3
-        group-hover:translate-y-3
-        group-hover:-rotate-2
-        group-hover:scale-105
+          group-hover:-translate-x-3
+          group-hover:translate-y-3
+          group-hover:-rotate-2
+          group-hover:scale-105
 
-        group-even:group-hover:translate-x-3
-        group-even:group-hover:-translate-y-3
-        group-even:group-hover:rotate-2
-        "
+          group-even:group-hover:translate-x-3
+          group-even:group-hover:-translate-y-3
+          group-even:group-hover:rotate-2
+          "
         />
       </section>
     </motion.div>
