4a874c2aab5268f2e2a0ddebf4415eceb8bc56d4
Timothy Japit <timothy.japit@gmail.com>
2024-05-02 04:48:59 +0700

    style: mobile - bottom navbar and moved theme-switch to middle

--- components/header.tsx
+++ components/header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
   return (
     <header className="z-50 relative">
       <motion.div
-        className="fixed top-0 left-1/2  h-[4.5rem] w-full 
+        className="fixed bottom-0 left-1/2  h-[4.5rem] w-full 
         rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 
         shadow-lg shadow-black/[.03] backdrop-blur-[0.5rem] 
         sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full
@@ -29,8 +29,8 @@ export default function Header() {
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
       <nav
-        className="py-2 flex fixed top-[.15rem] left-1/2 h-12 -translate-x-1/2 
-        sm:top-[1.7rem] sm:h-[initial] sm:py-0"
+        className="py-2 flex fixed bottom-5 left-1/2 h-12 -translate-x-1/2 
+        sm:bottom-auto sm:top-[1.7rem] sm:h-[initial] sm:py-0"
       >
         <ul
           className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 
--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -8,9 +8,11 @@ export default function ThemeSwitch() {
 
   return (
     <button
-      className="fixed bottom-5 right-5 
+      className="fixed bottom-1/2 -right-6
+      sm:bottom-5 sm:right-5
+      pl-2 sm:pl-0
       w-[3rem] h-[3rem]
-      flex items-center justify-center
+      flex items-center justify-start sm:justify-center
       bg-white/80 backdrop-blur-sm borderBlack rounded-full shadow-2xl outline-black
       hover:scale-125 active:scale-105 transition-all
       dark:bg-gray-950/80 dark:border-white/10 dark:hover:text-white
